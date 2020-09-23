34d7812dbfafa6f33cfe873b70948b1cd2ea39f8
antonlavrinov <antonlavrinov@users.noreply.github.com>
2020-09-23 10:15:24 +0000

    Deploying to gatsby-build from  @ 6884e24750e0ceaf0571bbadfa51994553b1508a 🚀

--- sw.js
+++ sw.js
@@ -232,14 +232,14 @@ self.__precacheManifest = [
   },
   {
     "url": "graphic-system-vhs/index.html",
-    "revision": "8becc61391b592d554f57c6e01920836"
+    "revision": "c360476680f8776d6c2bd90d5846cc82"
   },
   {
     "url": "idb-keyval-3.2.0-iife.min.js"
   },
   {
     "url": "index.html",
-    "revision": "4e9e274678c9aad5603029993ec69cfa"
+    "revision": "612803d9a05f97f8f7ac032cabdef90b"
   },
   {
     "url": "kak-oformit-abzac/index.html",
@@ -271,15 +271,15 @@ self.__precacheManifest = [
   },
   {
     "url": "osoznannaya-podborka-freelance/index.html",
-    "revision": "aa579027970910a88c3cc633bb72d527"
+    "revision": "6cba7802bbc67970725f142b1bf6a00d"
   },
   {
     "url": "osoznannaya-podborka-graphdesign/index.html",
-    "revision": "4b6cea254a6766bb1da0d7af4650164a"
+    "revision": "2f9eb7e55de366ddbaa45c3f8414925a"
   },
   {
     "url": "osoznanny-graph-design/index.html",
-    "revision": "6faab292ef99250b07f756068affc22f"
+    "revision": "f072c104256b209ca9f228ca3c9e6de5"
   },
   {
     "url": "page-data/404.html/page-data.json",
@@ -303,7 +303,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/index/page-data.json",
-    "revision": "ca6b629cf60abb220ee36ee486b0ffbe"
+    "revision": "dc722d1f90c5bb366bfdd2e6d255d7fe"
   },
   {
     "url": "page-data/kak-oformit-abzac/page-data.json",
