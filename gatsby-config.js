27ff96544e502643822179a225a0f1a0b6e73650
antonlavrinov <alv.anton.lavrinov@gmail.com>
2020-09-26 01:43:18 +0300

    yandex metrika

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,24 @@ module.exports = {
     },
     
     `gatsby-transformer-sharp`,
-    
+
+    {
+      resolve: `gatsby-plugin-yandex-metrika`,
+      options: {
+        // The ID of yandex metrika.
+        trackingId: 67665898,
+        // Enabled a webvisor. The default value is `false`.
+        webvisor: true,
+        // Enables tracking a hash in URL. The default value is `false`.
+        trackHash: false,
+        // Defines where to place the tracking script - `false` means before body (slower loading, more hits)
+        // and `true` means after the body (faster loading, less hits). The default value is `false`.
+        afterBody: false,
+        // Use `defer` attribute of metrika script. If set to `false` - script will be loaded with `async` attribute.
+        // Async enables earlier loading of the metrika but it can negatively affect page loading speed. The default value is `false`.
+        defer: false,
+      },
+    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-contentful`,
