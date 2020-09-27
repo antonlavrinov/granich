63d9f5471d5b36eb13c1c15d0a57e8976c24ea56
antonlavrinov <alv.anton.lavrinov@gmail.com>
2020-09-27 21:02:00 +0300

    test1

--- src/pages/osoznanny-graph-design.jsx
+++ src/pages/osoznanny-graph-design.jsx
@@ -237,9 +237,9 @@ const OsoznannyGraphDesignPage = ({data}) => (
     <CourseCommitment/>
     {/* <CoursePortfolio dataHeader={data.portfolioHeader} posters={data.portfolioPosters} multiPages={data.portfolioMultipage}/> */}
     
-    <CourseTrainingPath data={data.trainingPath}/>
+    {/* <CourseTrainingPath data={data.trainingPath}/> */}
     <CourseCurriculum dataHeader={data.curriculumHeader} data={data.curriculum}/>
-    <CourseFeatures data={data.features}/>
+    {/* <CourseFeatures data={data.features}/> */}
     <CourseReviews dataHeader={data.reviewsHeader} data={data.reviews}/>
     <div id="participation-section"></div>
     {data.offer.courseStatus ? (
