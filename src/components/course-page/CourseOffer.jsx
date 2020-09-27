2e815a6b75dead10486fb4f6f28904509f6d13ee
antonlavrinov <alv.anton.lavrinov@gmail.com>
2020-09-28 02:05:32 +0300

    no base 64

--- src/components/course-page/CourseOffer.jsx
+++ src/components/course-page/CourseOffer.jsx
@@ -464,7 +464,7 @@ const CourseOffer = ({data}) => {
                     <CourseOfferTitle type={data.courseType}>
                         {documentToReactComponents(data.courseMainTitle.json)}
                     </CourseOfferTitle>
-                    <CourseOfferMainImage loading="eager" fadeIn={false} fluid={data.courseMainImage.fluid}/>
+                    <CourseOfferMainImage fluid={data.courseMainImage.fluid}/>
                     <CourseOfferDescr type={data.courseType}>
                         {data.courseDescr}
                     </CourseOfferDescr>
