50421b64641cbd78db6d3690967d2845d3f2c186
antonlavrinov <alv.anton.lavrinov@gmail.com>
2020-09-28 02:12:12 +0300

    no base 64

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,7 +27,7 @@ export const contentfulQuery = graphql`
               }
               headerImageMobile {
                 fluid(maxWidth: 575) {
-                  ...GatsbyContentfulFluid_withWebp_noBase64
+                  ...GatsbyContentfulFluid_withWebp
                 }
               }
               headerSubtitleImage {
