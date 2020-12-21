07e7a37827d57cc74def445117082d70c350d54d
antonlavrinov <alv.anton.lavrinov@gmail.com>
2020-12-21 12:25:00 +0300

    sddsdsdsds

--- src/pages/private-access-readymag.jsx
+++ src/pages/private-access-readymag.jsx
@@ -3,39 +3,21 @@ import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import CourseOffer from '../components/course-page/CourseOffer';
 import Header from "../components/Header"
-// import CourseExplanations from "../components/course-page/CourseExplanations"
-// import CourseExample from "../components/course-page/CourseExample"
-// // import CourseForWhom from "../components/course-page/CourseForWhom"
-// import CoursePortfolio from "../components/course-page/CoursePortfolio"
-// import CourseTrainingPath from "../components/course-page/CourseTrainingPath"
-import CourseCurriculum from "../components/course-page/CourseCurriculum"
-// import CourseFeatures from "../components/course-page/CourseFeatures"
-// import CourseReviews from "../components/course-page/CourseReviews"
-import CourseParticipation from "../components/course-page/CourseParticipation"
-// import CourseCommitment from "../components/course-page/CourseCommitment"
+// import CourseCurriculum from "../components/course-page/CourseCurriculum"
+// import CourseParticipation from "../components/course-page/CourseParticipation"
 import { graphql } from 'gatsby';
-// import CourseAnswers from "../components/course-page/CourseAnswers";
-// import Mailing from '../components/Mailing';
-// import CourseExplain from '../components/course-page/CourseExplain'
 import ogImage from '../assets/images/seo/conscious-freelance.jpg';
 import ogImageVK from '../assets/images/seo/vk/conscious-freelance.jpg';
-// import CourseIndividualSupport from "../components/course-page/CourseIndividualSupport";
-// import CourseTimetable from "../components/course-page/CourseTimetable";
-// import CourseAftermath from "../components/course-page/CourseAftermath"
-// import CourseOpportunities from "../components/course-page/CourseOpportunities";
+// import CourseForWhom from "../components/course-page/freelance/ForWhom";
+// import CourseFeatures from "../components/course-page/CourseFeatures";
+// import FreelanceHardWork from "../components/course-page/freelance/FreelanceHardWork";
+// import CourseFreelanceBanner from "../components/course-page/freelance/CourseFreelanceBanner";
 // import CoursePaymentChoices from "../components/course-page/CoursePaymentChoices";
-// import CourseBanner from "../components/course-page/CourseBanner";
-import CourseForWhom from "../components/course-page/freelance/ForWhom";
-import CourseFeatures from "../components/course-page/CourseFeatures";
-import FreelanceHardWork from "../components/course-page/freelance/FreelanceHardWork";
-import CourseFreelanceBanner from "../components/course-page/freelance/CourseFreelanceBanner";
-import CoursePaymentChoices from "../components/course-page/CoursePaymentChoices";
-import CourseTimetableFreelance from "../components/course-page/freelance/CourseTimetableFreelance";
 
 export const contentfulQuery = graphql`
 
-    query freelanceQuery {
-        offer:   contentfulGranichCourse(courseTitle: {eq: "Осознанный Фриланс"}) {
+    query readymagQuery {
+        offer:   contentfulGranichCourse(courseTitle: {eq: "Осознанный Медиадизайн"}) {
           courseMainImage {
             fluid(maxWidth: 850, quality: 90) {
               ...GatsbyContentfulFluid_withWebp
@@ -130,37 +112,36 @@ export const contentfulQuery = graphql`
 
 
 
-const OsoznannyFreelancePage = ({ data }) => {
+const ReadymagPage = ({ data }) => {
 
   return (
     <Layout>
       <Header type={'dark'} />
-      <SEO title="Осознанный Фриланс"
+      <SEO title="Осознанный Медиадизайн"
         description="Курс Осознанный Фриланс от Вадима Гранича. Онлайн-школа Granich."
         socialDescription="Курс для творцов, которые хотят преуспеть на фрилансе"
         keywords={['фриланс', 'осознанный', 'гранич', 'freelance', 'granich', 'conscious']}
         ogImage={ogImage}
         ogImageVk={ogImageVK}
-        url="https://granich.design/conscious-freelance" />
+        url="https://granich.design/readymag" />
       <div className="section-top-block"></div>
       <CourseOffer data={data.offer} />
 
-      <CourseForWhom />
+      {/* <CourseForWhom /> */}
 
-      <CourseCurriculum compressedHeader dataHeader={data.curriculumHeader} data={data.curriculum} />
-      <CourseTimetableFreelance />
-      <CourseFeatures title="Особенности курса" data={data.features} />
-      <FreelanceHardWork />
+      {/* <CourseCurriculum compressedHeader dataHeader={data.curriculumHeader} data={data.curriculum} /> */}
+      {/* <CourseFeatures title="Особенности курса" data={data.features} /> */}
+      {/* <FreelanceHardWork /> */}
       <div id="participation-section"></div>
-      <CourseParticipation
-        data={data.offer}
-        formId={`ltForm5127304`}
-        formAction={`https://school.granich.design/pl/lite/block-public/process-html?id=935677483`}
-        googleAnaliticsCategory={`Отправка формы Осознанный Фриланс`}
-        additionalTags={['Только 6 учеников']}
-      />
-      <CoursePaymentChoices paymentsAvailable={{ 'credit': true, 'corporate': false, 'abroad': false }} />
-      <CourseFreelanceBanner />
+      {/* <CourseParticipation
+                data={data.offer}
+                formId={`ltForm5127304`}
+                formAction={`https://school.granich.design/pl/lite/block-public/process-html?id=935677483`}
+                googleAnaliticsCategory={`Отправка формы Осознанный Фриланс`}
+                additionalTags={['Только 6 учеников']}
+            /> */}
+      {/* <CoursePaymentChoices paymentsAvailable={{ 'credit': true, 'corporate': false, 'abroad': false }} /> */}
+      {/* <CourseFreelanceBanner /> */}
 
 
     </Layout>
@@ -170,4 +151,4 @@ const OsoznannyFreelancePage = ({ data }) => {
 
 
 
-export default OsoznannyFreelancePage
+export default ReadymagPage
