f8805c07f545e6056ec441646d8ec714d646a298
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-03-05 20:49:28 +0300

    updated freelance page participation section

--- src/components/course-page/freelance/FreelanceSkills.jsx
+++ src/components/course-page/freelance/FreelanceSkills.jsx
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 import LoneFreelancer from '../../../assets/svgs/freelance/freelance-skills.svg';
 
 const SkillsSection = styled.section`
-    margin-bottom: 4vw;
-    @media only screen and (max-width: 575px) {
-        margin-bottom: 5vw;
-    }
+    // margin-bottom: 4vw;
+    // @media only screen and (max-width: 575px) {
+    //     margin-bottom: 5vw;
+    // }
 `
 
 const SkillsWrapper = styled.section`
