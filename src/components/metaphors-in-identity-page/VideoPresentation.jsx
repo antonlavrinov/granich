f7e39a4554b06f07b0ac76ec6461b4395d305f07
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-03-26 01:41:15 +0300

    update

---
+++ src/components/metaphors-in-identity-page/VideoPresentation.jsx
@@ -0,0 +1,198 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Container } from '../style';
+import PotokIcon from '../../assets/svgs/graph-design/graph-design-potok-icon.svg';
+import DateIcon from '../../assets/svgs/graph-design/graph-design-date-icon.svg';
+
+
+const Potok = styled(props => <PotokIcon {...props} />)`
+    width: 1.3vw;
+    height: 1.3vw;
+    margin-right: 0.3vw;
+    fill: white;
+    @media only screen and (max-width: 575px) {
+        width: 4.3vw;
+        height: 4vw;
+        margin-right: 1vw;
+        margin-top: -0.3vw;
+    }
+`
+
+const Date = styled(props => <DateIcon {...props} />)`
+    width: 1.3vw;
+    height: 1.3vw;
+    fill: var(--granich-red);
+    margin-right: 0.3vw;
+    @media only screen and (max-width: 575px) {
+        width: 4.3vw;
+        height: 4vw;
+        margin-right: 1vw;
+        margin-top: -0.8vw;
+    }
+`
+
+const VideoPresentationOfferSection = styled.section`
+    margin-bottom: 2vw;
+`
+
+const VideoPresentationOfferWrapper = styled.div`
+    background: white;
+    // background: #2A2A2A;
+    border-radius: 0.6vw;
+    padding: 4vw;
+
+    @media only screen and (max-width: 575px) {
+
+        border-radius: 2.5vw;
+    }
+`
+
+const YoutubeBlock = styled.div`
+    // width: 100%;
+    height: 45vw;
+    // padding: 3vw;
+    position: relative;
+    
+    iframe {
+        border: 2px solid rgba(0,0,0,0.2);
+        position: relative;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        height: 100%;
+    }
+    @media only screen and (max-width: 575px) {
+        padding: 4vw 6.6vw;
+        height: auto;
+        iframe {
+            
+            position: relative;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            height: 43vw;
+        }
+    }
+    
+
+
+`
+
+const Title = styled.h1`
+
+    font-size: 6.9vw;
+    font-weight: 500;
+    font-family: EB Garamond;
+    letter-spacing: -0.1rem;
+    line-height: 0.85;
+    margin: 0;
+    margin-bottom: 3vw;
+    text-align: center;
+    span {
+        font-family: EB Garamond;
+        font-style: italic;
+        font-size: 6.5vw;
+        letter-spacing: -0.1rem;
+        display: block;
+    }
+`
+
+const TagList = styled.div`
+    display: flex;
+    margin-bottom: 2.1vw; 
+    align-items: flex-start;
+    justify-content: center;
+    flex-wrap: wrap;
+    position: relative;
+    z-index: 1;
+    @media only screen and (max-width: 575px) {
+        padding: 0 3.5vw;
+        max-width: 100%;
+    }
+`
+
+const Tag = styled.div`
+    font-size: 1.2vw;
+    display: flex;
+    color: var(--granich-red);
+    border: 1.5px solid var(--granich-red);
+    padding: 0.3vw 0.8vw;
+    border-radius: 100vw;
+    margin-right: 0.5vw;
+    margin-bottom: 1vw;
+    font-weight: 500;
+    align-items: center;
+    @media only screen and (max-width: 575px) {
+        font-size: 3.7vw;
+        padding: 1.8vw 2.5vw 1.6vw;
+        margin-right: 3vw;
+        white-space: nowrap;
+        margin-bottom: 3vw;
+        line-height: 1;
+    }
+`
+
+const MainTag = styled.div`
+    font-size: 1.2vw;
+    display: flex;
+    color: white;
+    background: var(--granich-red);
+    border: 1.5px solid var(--granich-red);
+    padding: 0.3vw 0.8vw;
+    border-radius: 100vw;
+    margin-right: 0.5vw;
+    font-weight: 500;
+    @media only screen and (max-width: 575px) {
+        font-size: 3.7vw;
+        padding: 1.6vw 2.5vw;
+        margin-right: 3vw;
+        white-space: nowrap;
+        margin-bottom: 3vw;
+    }
+`
+
+const VideoPresentationOfferHeader = styled.div`
+
+`
+
+
+
+
+
+const VideoPresentationOffer = ({data}) => {
+    return (
+        <VideoPresentationOfferSection>
+            <Container>
+                <VideoPresentationOfferWrapper>
+                    <VideoPresentationOfferHeader>
+                        <Title>
+                            <span>Графические метафоры </span>как путь в айдентику
+                        </Title>
+                        <TagList>
+                            {data.courseStream && <MainTag><Potok />{data.courseStream} поток</MainTag>}
+                            {data.courseStartAndEnd && <Tag><Date />{data.courseStartAndEnd}</Tag>}
+                            {data.courseTags.map((tag, idx) => {
+                                return (
+                                    <Tag key={idx}>{tag}</Tag>
+                                )
+                            })}
+                        </TagList>
+                    </VideoPresentationOfferHeader>
+                    <YoutubeBlock>
+                        <div className="loadingio-spinner-rolling-ta5f1nqy5p">
+                            <div className="ldio-6b142d04hhl">
+                                <div></div>
+                            </div>
+                        </div>
+                        <iframe title="youtubeFrame" width="100%"  src={`https://www.youtube.com/embed/F4DCWCKmtyY?vq=hd1080`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </YoutubeBlock>
+                </VideoPresentationOfferWrapper>
+                
+            </Container>   
+        </VideoPresentationOfferSection>
+    )
+}
+
+export default VideoPresentationOffer
---
+++ src/pages/graphic-metaphors-in-identity.jsx
@@ -0,0 +1,102 @@
+import React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import CourseOffer from '../components/course-page/CourseOffer';
+import Header from "../components/Header"
+// import CourseCurriculum from "../components/course-page/CourseCurriculum"
+import CourseParticipation from "../components/course-page/CourseParticipation"
+import { graphql } from 'gatsby';
+import ogImage from '../assets/images/seo/conscious-freelance.jpg';
+import ogImageVK from '../assets/images/seo/vk/conscious-freelance.jpg';
+import VideoPresentationOffer from "../components/metaphors-in-identity-page/VideoPresentation";
+// import CourseForWhom from "../components/course-page/freelance/ForWhom";
+// import CourseFeatures from "../components/course-page/CourseFeatures";
+// import FreelanceHardWork from "../components/course-page/freelance/FreelanceHardWork";
+// import CourseFreelanceBanner from "../components/course-page/freelance/CourseFreelanceBanner";
+// import CoursePaymentChoices from "../components/course-page/payment-choices/CoursePaymentChoices";
+// import Combination from "../components/course-page/freelance/Combination";
+// import FreelanceOffer from "../components/course-page/freelance/FreelanceOffer";
+// import FreelanceBefore from "../components/course-page/freelance/FreelanceBefore"
+// import FreelanceAfter from "../components/course-page/freelance/FreelanceAfter"
+// import FreelanceProblem from "../components/course-page/freelance/FreelanceProblem"
+// import FreelanceSkills from "../components/course-page/freelance/FreelanceSkills"
+// import FreelanceParticipation from "../components/course-page/freelance/FreelanceParticipation"
+
+export const contentfulQuery = graphql`
+
+    query graphicMetaphorsQuery {
+        offer:   contentfulGranichCourse(courseTitle: {eq: "Графические метафоры как путь в айдентику"}) {
+          courseMainImage {
+            fluid(maxWidth: 850, quality: 90) {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+          courseTitle
+          courseMainTitle {
+            json
+          }
+          courseDescr
+          courseStart
+          courseDuration
+          courseStartAndEnd
+          courseStream
+          courseStatus
+          courseType
+          coursePrice
+          courseTags
+          coursePostersCollection {
+            fluid(maxWidth: 600, quality: 100) {
+              ...GatsbyContentfulFluid_withWebp
+            }
+            resize(width: 600, quality: 100) {
+              src
+            }
+          }
+          childContentfulGranichCourseCoursePolicyRichTextNode {
+            json
+          }
+        }
+
+
+
+    }
+  `
+
+
+
+const GraphicMetaphorsInIdentityPage = ({ data }) => {
+
+  return (
+    <Layout>
+      <Header type={'dark'} />
+      <SEO title="Графические метафоры как путь в айдентику"
+        description="Курс Графические метафоры как путь в айдентику от Вадима Гранича. Онлайн-школа Granich."
+        socialDescription="Курс для творцов, которые хотят преуспеть на фрилансе"
+        keywords={['фриланс', 'осознанный', 'гранич', 'freelance', 'granich', 'conscious']}
+        ogImage={ogImage}
+        ogImageVk={ogImageVK}
+        url="https://granich.design/conscious-freelance" />
+      <div className="section-top-block"></div>
+      {/* <CourseOffer data={data.offer} /> */}
+      <VideoPresentationOffer data={data.offer}/>
+      <CourseParticipation
+        data={data.offer}
+        formId={`ltForm6865073`}
+        formAction={`https://school.granich.design/pl/lite/block-public/process-html?id=855573236`}
+        googleAnaliticsCategory={`Отправка формы Осознанный Графдизайн`}
+        additionalTags={['Интенсивный', 'С нуля']} />
+      {/* <FreelanceOffer /> */}
+
+      {/* <FreelanceHardWork />
+      <FreelanceSkills /> */}
+      <div id="participation-section"></div>
+      {/* <FreelanceParticipation courseData={data.offer} /> */}
+
+    </Layout>
+  )
+}
+
+
+
+
+export default GraphicMetaphorsInIdentityPage
