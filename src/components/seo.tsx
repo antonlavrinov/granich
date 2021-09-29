094359984be1ca439b135b44d6cd21586599c51b
antonlavrinov <alv.anton.lavrinov@gmail.com>
2021-09-29 15:39:18 +0300

    added facebook pixel

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -140,6 +140,18 @@ const SEO: React.FC<Props> = ({
       meta={defaultMeta.concat(propMeta)}
     >
       {url && <link rel="canonical" href={metaOgUrl} />}
+      <script>
+        {`!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '176905957827936');
+        fbq('track', 'PageView');`}
+      </script>
     </Helmet>
   )
 }
